4ff7d3f8590eded30da951c12dac6fa074da19c9
sgantzer12 <74564975+sgantzer12@users.noreply.github.com>
2024-10-29 11:12:59 +0100

    Deleted App and started rendering

--- MyGameMaker/MyGameEngine/MyWindow.cpp
+++ MyGameMaker/MyGameEngine/MyWindow.cpp
@@ -1,56 +1,55 @@
-#include <SDL2/SDL.h>
+#include <exception>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_opengl.h>
 #include "MyWindow.h"
-#include <stdexcept>
+using namespace std;
 
-Window::Window(const char* title, unsigned short width, unsigned short height)
-    : _width(width), _height(height) {
-    Open(title, width, height);
+MyWindow::MyWindow(const char* title, unsigned short width, unsigned short height) {
+    open(title, width, height);
 }
 
-Window::~Window() {
-    Close();
+MyWindow::~MyWindow() {
+    close();
 }
 
-void Window::Open(const char* title, unsigned short width, unsigned short height) {
-    if (IsOpen()) return;
-
+void MyWindow::open(const char* title, unsigned short width, unsigned short height) {
+    if (isOpen()) return;
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-
     _window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
-    if (!_window) throw std::runtime_error("Failed to create SDL window");
+    if (!_window) throw exception(SDL_GetError());
 
-    _context = SDL_GL_CreateContext(_window);
-    if (!_context) throw std::runtime_error("Failed to create OpenGL context");
-
-    if (SDL_GL_MakeCurrent(_window, _context) != 0) throw std::runtime_error("Failed to set OpenGL context");
-    if (SDL_GL_SetSwapInterval(1) != 0) throw std::runtime_error("Failed to enable VSync");
+    _ctx = SDL_GL_CreateContext(_window);
+    if (!_ctx) throw exception(SDL_GetError());
+    if (SDL_GL_MakeCurrent(_window, _ctx) != 0) throw exception(SDL_GetError());
+    if (SDL_GL_SetSwapInterval(1) != 0) throw exception(SDL_GetError());
 }
 
-void Window::Close() {
-    if (!IsOpen()) return;
+void MyWindow::close() {
+    if (!isOpen()) return;
 
-    SDL_GL_DeleteContext(_context);
-    SDL_DestroyWindow(_window);
-    _context = nullptr;
+    SDL_GL_DeleteContext(_ctx);
+    _ctx = nullptr;
+
+    SDL_DestroyWindow(static_cast<SDL_Window*>(_window));
     _window = nullptr;
 }
 
-bool Window::ProcessEvents() {
+void MyWindow::swapBuffers() const {
+    SDL_GL_SwapWindow(static_cast<SDL_Window*>(_window));
+}
+
+bool MyWindow::processEvents(IEventProcessor* event_processor) {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT) {
-            Close();
-            return false;
+        if (event_processor) event_processor->processEvent(e);
+        switch (e.type) {
+        case SDL_QUIT: close(); return false;
         }
     }
     return true;
-}
-
-void Window::SwapBuffers() const {
-    SDL_GL_SwapWindow(_window);
 }
--- MyGameMaker/MyGameEngine/MyWindow.h
+++ MyGameMaker/MyGameEngine/MyWindow.h
@@ -3,24 +3,39 @@
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_events.h>
 
-class Window {
-    SDL_Window* _window = nullptr;
-    SDL_GLContext _context = nullptr;
-    unsigned short _width;
-    unsigned short _height;
 
+class IEventProcessor {
 public:
-    Window(const char* title, unsigned short width, unsigned short height);
-    ~Window();
-
-    void Open(const char* title, unsigned short width, unsigned short height);
-    void Close();
-    bool IsOpen() const { return _window != nullptr; }
-    bool ProcessEvents();
-    void SwapBuffers() const;
-
-    int width() const { return _width; }
-    int height() const { return _height; }
-    double aspect() const { return static_cast<double>(_width) / _height; }
+	virtual void processEvent(const SDL_Event& event) = 0;
 };
 
+class MyWindow {
+
+	SDL_Window* _window = nullptr;
+	void* _ctx = nullptr;
+
+	unsigned short _width = 800;
+	unsigned short _height = 600;
+
+public:
+	int width() const { return _width; }
+	int height() const { return _height; }
+	double aspect() const { return static_cast<double>(_width) / _height; }
+
+	auto* windowPtr() const { return _window; }
+	auto* contextPtr() const { return _ctx; }
+
+	MyWindow(const char* title, unsigned short width, unsigned short height);
+	MyWindow(MyWindow&&) noexcept = delete;
+	MyWindow(const MyWindow&) = delete;
+	MyWindow& operator=(const MyWindow&) = delete;
+	~MyWindow();
+
+	void open(const char* title, unsigned short width, unsigned short height);
+	void close();
+	bool isOpen() const { return _window; }
+
+	bool processEvents(IEventProcessor* event_processor = nullptr);
+	void swapBuffers() const;
+
+};
